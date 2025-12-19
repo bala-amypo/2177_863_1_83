fdd21562761c36d892c4d09df3b19c2bcf43d374
bala-amypo <bala@amypo.in>
2025-12-19 04:32:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -1,3 +1,14 @@
 package om.example.demo.entity;
 
 import jakarta.persistence.*;
+import java.time.LocalDteTime;
+
+@Entity
+@Table(name='vendors',uniqueConstraints=@UnigueConstraint(columnNames="name"))
+public class Vendor{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+}
