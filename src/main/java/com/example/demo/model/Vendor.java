ac24c42eed423bd4c0b3299ffa09b685d06f9b3f
bala-amypo <bala@amypo.in>
2025-12-19 04:35:30 +0000

    User commit

--- src/main/java/com/example/demo/model/Vendor.java
+++ src/main/java/com/example/demo/model/Vendor.java
@@ -10,5 +10,9 @@ public class Vendor{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    
+
+    @Column(nullable=false,unique=true)
+    private String name;
+
+    private String contactEmail
 }
