c390896a4af5a491ca476072c4ba51c249b4b88c
bala-amypo <bala@amypo.in>
2025-12-19 04:20:26 +0000

    User commit

--- src/main/java/com/example/demo/Servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/Servlet/SimpleStatusServlet.java
@@ -6,4 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@
+@Webserver("/status")
+public class SimpleStatusServlet extends HttpServlet{
+
+    @Overide
+    protected void doGet(HttpServletRequest r,HttpServletResponce rep)throws Se
+}
