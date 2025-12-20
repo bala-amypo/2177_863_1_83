583c4581288bf416caef920b0048de49df33f283
bala-amypo <bala@amypo.in>
2025-12-20 16:12:51 +0000

    User commit

--- src/main/java/com/example/demo/model/DeliveryEvaluation.java
+++ src/main/java/com/example/demo/model/DeliveryEvaluation.java
@@ -7,51 +7,83 @@ import java.time.LocalDate;
 @Table(name = "delivery_evaluations")
 public class DeliveryEvaluation {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(optional = false)
-    private Vendor vendor;
+	@ManyToOne(optional = false)
+	private Vendor vendor;
 
-    @ManyToOne(optional = false)
-    private SLARequirement slaRequirement;
+	@ManyToOne(optional = false)
+	private SLARequirement slaRequirement;
 
-    private Integer actualDeliveryDays;
-    private Double qualityScore;
-    private LocalDate evaluationDate;
+	private Integer actualDeliveryDays;
+	private Double qualityScore;
+	private LocalDate evaluationDate;
 
-    private Boolean meetsDeliveryTarget;
-    private Boolean meetsQualityTarget;
+	private Boolean meetsDeliveryTarget;
+	private Boolean meetsQualityTarget;
 
-    public DeliveryEvaluation() {}
+	public DeliveryEvaluation() {}
 
-    public DeliveryEvaluation(Vendor vendor,
-                              SLARequirement slaRequirement,
-                              Integer actualDeliveryDays,
-                              Double qualityScore,
-                              LocalDate evaluationDate) {
-        this.vendor = vendor;
-        this.slaRequirement = slaRequirement;
-        this.actualDeliveryDays = actualDeliveryDays;
-        this.qualityScore = qualityScore;
-        this.evaluationDate = evaluationDate;
-    }
+	public DeliveryEvaluation(Vendor vendor,
+	                          SLARequirement slaRequirement,
+	                          Integer actualDeliveryDays,
+	                          Double qualityScore,
+	                          LocalDate evaluationDate) {
+		this.vendor = vendor;
+		this.slaRequirement = slaRequirement;
+		this.actualDeliveryDays = actualDeliveryDays;
+		this.qualityScore = qualityScore;
+		this.evaluationDate = evaluationDate;
+	}
 
-    public Long getId() { return id; }
-    public Vendor getVendor() { return vendor; }
-    public SLARequirement getSlaRequirement() { return slaRequirement; }
-    public Integer getActualDeliveryDays() { return actualDeliveryDays; }
-    public Double getQualityScore() { return qualityScore; }
+	// Getters
+	public Long getId() {
+		return id;
+	}
+	public Vendor getVendor() {
+		return vendor;
+	}
+	public SLARequirement getSlaRequirement() {
+		return slaRequirement;
+	}
+	public Integer getActualDeliveryDays() {
+		return actualDeliveryDays;
+	}
+	public Double getQualityScore() {
+		return qualityScore;
+	}
+	public LocalDate getEvaluationDate() {
+		return evaluationDate;
+	}
+	public Boolean getMeetsDeliveryTarget() {
+		return meetsDeliveryTarget;
+	}
+	public Boolean getMeetsQualityTarget() {
+		return meetsQualityTarget;
+	}
 
-    public Boolean isMeetsDeliveryTarget() { return meetsDeliveryTarget; }
-    public Boolean isMeetsQualityTarget() { return meetsQualityTarget; }
-
-    public void setMeetsDeliveryTarget(Boolean meetsDeliveryTarget) {
-        this.meetsDeliveryTarget = meetsDeliveryTarget;
-    }
-
-    public void setMeetsQualityTarget(Boolean meetsQualityTarget) {
-        this.meetsQualityTarget = meetsQualityTarget;
-    }
+	// Setters
+	public void setVendor(Vendor vendor) {
+		this.vendor = vendor;
+	}
+	public void setSlaRequirement(SLARequirement slaRequirement) {
+		this.slaRequirement = slaRequirement;
+	}
+	public void setActualDeliveryDays(Integer actualDeliveryDays) {
+		this.actualDeliveryDays = actualDeliveryDays;
+	}
+	public void setQualityScore(Double qualityScore) {
+		this.qualityScore = qualityScore;
+	}
+	public void setEvaluationDate(LocalDate evaluationDate) {
+		this.evaluationDate = evaluationDate;
+	}
+	public void setMeetsDeliveryTarget(Boolean meetsDeliveryTarget) {
+		this.meetsDeliveryTarget = meetsDeliveryTarget;
+	}
+	public void setMeetsQualityTarget(Boolean meetsQualityTarget) {
+		this.meetsQualityTarget = meetsQualityTarget;
+	}
 }
