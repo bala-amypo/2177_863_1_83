6d34ec9703e46e1d70c0808faf009b92c71fe688
bala-amypo <bala@amypo.in>
2025-12-20 14:59:52 +0000

    User commit

--- src/main/java/com/example/demo/reposiory/VendorPerformanceScoreRepository.java
+++ src/main/java/com/example/demo/reposiory/VendorPerformanceScoreRepository.java
@@ -2,11 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.VendorPerformanceScore;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface VendorPerformanceScoreRepository
         extends JpaRepository<VendorPerformanceScore, Long> {
 
     List<VendorPerformanceScore>
-    findByVendorOrderByCalculatedAtDesc(Long vendorId);
+    findByVendor_IdOrderByCalculatedAtDesc(Long vendorId);
 }
