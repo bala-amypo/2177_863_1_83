13c399c315048620665c42a704fc924d6c885e82
bala-amypo <bala@amypo.in>
2025-12-20 18:08:52 +0000

    User commit

--- src/main/java/com/example/demo/reposiory/DeliveryEvaluationRepository.java
+++ src/main/java/com/example/demo/reposiory/DeliveryEvaluationRepository.java
@@ -1,13 +1,12 @@
-package com.example.demo.repository;
-
 import com.example.demo.model.DeliveryEvaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation, Long> {
 
+    // Correct method names
     List<DeliveryEvaluation> findByVendor_Id(Long vendorId);
-
     List<DeliveryEvaluation> findBySlaRequirement_Id(Long requirementId);
 }
