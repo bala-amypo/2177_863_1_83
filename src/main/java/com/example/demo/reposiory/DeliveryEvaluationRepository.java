3751dcd07287839fbd92d7e2c7168e1d5c4d731d
bala-amypo <bala@amypo.in>
2025-12-24 05:15:56 +0000

    User commit

--- src/main/java/com/example/demo/reposiory/DeliveryEvaluationRepository.java
+++ src/main/java/com/example/demo/reposiory/DeliveryEvaluationRepository.java
@@ -1,10 +1,37 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.DeliveryEvaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.entity.*;
+
 import java.util.List;
 
 public interface DeliveryEvaluationRepository extends JpaRepository<DeliveryEvaluation, Long> {
-    List<DeliveryEvaluation> findByVendor_Id(Long vendorId);
-    List<DeliveryEvaluation> findBySlaRequirement_Id(Long slaRequirementId);
+
+    List<DeliveryEvaluation> findByVendorId(Long vendorId);
+
+    List<DeliveryEvaluation> findBySlaRequirementId(Long slaId);
+
+     @Query
+    @Query("""
+           SELECT d FROM DeliveryEvaluation d
+           WHERE d.vendor = :vendor
+           AND d.score >= :minScore
+           """)
+    List<DeliveryEvaluation> findHighQualityDeliveries(
+            @Param("vendor") Vendor vendor,
+            @Param("minScore") Double minScore
+    );
+
+    @Query
+    @Query("""
+           SELECT d FROM DeliveryEvaluation d
+           WHERE d.slaRequirement = :sla
+           AND d.onTime = true
+           """)
+    List<DeliveryEvaluation> findOnTimeDeliveries(
+            @Param("sla") SLARequirement sla
+    );
 }
