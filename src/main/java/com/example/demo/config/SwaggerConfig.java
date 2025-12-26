9916e5a81557544c9c9ff65c29846311231227b6
bala-amypo <bala@amypo.in>
2025-12-26 08:45:29 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -5,13 +5,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -23,18 +16,7 @@ public class SwaggerConfig {
                 .servers(List.of(
                     new Server().url("https://9068.32procr.amypo.ai")
 
-                    SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        return new OpenAPI()
-                .components(
-                        new Components().addSecuritySchemes("bearerAuth", bearerAuth)
-                )
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth")
-                );
+                    
                 ));
         }
 }
