5864d5a9de6424dd579cb7ba76a99ea89ae1280a
bala-amypo <bala@amypo.in>
2025-12-27 06:40:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,47 +1,27 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.dto.AuthRequest;
-import com.example.demo.dto.AuthResponse;
-import com.example.demo.model.User;
-import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private final UserService userService;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthController(UserService userService,
-                          JwtTokenProvider jwtTokenProvider) {
+    public AuthController(UserService userService) {
         this.userService = userService;
-        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/register")
-    public User register(@RequestBody AuthRequest request) {
-        return userService.register(
-                request.getEmail(),
-                request.getPassword(),
-                "ADMIN"
-        );
+    public String register() {
+        userService.registerUser();
+        return "User registered successfully";
     }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest request) {
-        User user = userService.login(
-                request.getEmail(),
-                request.getPassword()
-        );
-
-        String token = jwtTokenProvider.createToken(
-                user.getEmail(),
-                user.getRole(),
-                user.getId()
-        );
-
-        return new AuthResponse(token);
+    public String login() {
+        userService.login();
+        return "User logged in successfully";
     }
 }
