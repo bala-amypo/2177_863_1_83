8925e167e30c91a41afa9bd267010d8206fbf71f
bala-amypo <bala@amypo.in>
2025-12-27 06:11:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,33 @@
+package com.example.demo.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repo;
+
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public User register(String email, String password, String role) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRole(role);
+        return repo.save(user);
+    }
+
+    @Override
+    public User login(String email, String password) {
+        return repo.findByEmail(email)
+                .filter(u -> u.getPassword().equals(password))
+                .orElseThrow(() ->
+                        new RuntimeException("Invalid credentials"));
+    }
+}
